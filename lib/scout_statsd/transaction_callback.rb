22af477493a1f89e7610c4ce53d971a24666154c
Mike Stewart <mike.stewart@introhive.com>
2018-05-02 20:07:06 -0300

    Use a timer for queue time
    scoutapp/scout_dogstatsd_ruby#1

--- lib/scout_statsd/transaction_callback.rb
+++ lib/scout_statsd/transaction_callback.rb
@@ -5,7 +5,7 @@ module ScoutStatsd
       StatsD.measure("#{ScoutStatsd.prefix}#{payload.transaction_type_slug}.duration_ms", payload.duration_ms)
 
       if payload.queue_time_ms
-        StatsD.gauge("#{ScoutStatsd.prefix}#{payload.transaction_type_slug}.queue_time_ms", payload.queue_time_ms)
+        StatsD.measure("#{ScoutStatsd.prefix}#{payload.transaction_type_slug}.queue_time_ms", payload.queue_time_ms)
       end
 
       if payload.error?
